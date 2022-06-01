50639a8623ee5f4e645d6f439fd1d20bdccee79d
NewChap2022 <irisyangshu@gmail.com>
2022-05-31 22:37:51 -0400

    5-day-forecast and display search history

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,9 @@ var userFormEl = document.querySelector("#user-form");
 var formInputEl = document.querySelector("#cityname");
 var cityNameEl = document.querySelector("#search-city-name");
 var currentWeatherEl = document.querySelector(".currentweather");
+var forecastEl = document.querySelector(".forecast");
+var searchHistoryEl = document.querySelector(".search-history");
+var searchHistory = [];
 var apiKey = "e10a1bcf5d67a6b3f71484bd7a8c46d2";
 
 var getCityWeather = function (city) {
@@ -9,22 +12,23 @@ var getCityWeather = function (city) {
     fetch(firstApiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data);
+                addNewSearchHistory(data);
                 displayCityName(data);
                 // https://api.openweathermap.org/data/2.5/onecall?lat=43.6534817&lon=-79.3839347&exclude=minutely,hourly,daily&appid=e10a1bcf5d67a6b3f71484bd7a8c46d2
                 var secondApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&exclude=minutely,hourly,alerts&units=metric&appid=" + apiKey;
-                fetch(secondApiUrl).then(function(response) {
+                fetch(secondApiUrl).then(function (response) {
                     if (response.ok) {
                         response.json().then(function (data) {
                             displayCurrentWeather(data);
+                            display5DayForecast(data);
                         })
                     } else {
                         alert("Error: the weather is not found!");
                     }
                 })
-                .catch(function(error) {
-                    alert("Unable to Connect to Open Weather Map. Please Try Again");
-                });
+                    .catch(function (error) {
+                        alert("Unable to Connect to Open Weather Map. Please Try Again");
+                    });
             })
         } else {
             alert("Error: " + city + " is not found!");
@@ -93,7 +97,7 @@ var displayCurrentWeather = function (data) {
     uvIndex.style.color = "white";
     if (uvi <= 2) {
         uvIndex.style.backgroundColor = "green";
-    } else if (uvi >=3 && uvi <=5) {
+    } else if (uvi >= 3 && uvi <= 5) {
         uvIndex.style.backgroundColor = "yellow";
     } else {
         uvIndex.style.backgroundColor = "red";
@@ -105,5 +109,78 @@ var displayCurrentWeather = function (data) {
     currentWeatherEl.append(dateEl, weatherDataEl);
 };
 
+var display5DayForecast = function (data) {
+    for (var i = 0; i < 5; i++) {
+        var forecastCardEl = document.createElement("div");
+        forecastCardEl.classList.add("forecast-card", "col-12", "col-sm-4", "col-lg");
+
+        var dateEl = document.createElement("p");
+        date = moment().add(i + 1, 'd').format("MMM Do YYYY");
+        dateEl.textContent = date;
+
+        var weatherIconEl = document.createElement("div");
+        var weatherIcon = document.createElement("img");
+        weatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
+        weatherIcon.setAttribute("alt", "current weather is " + data.daily[i].weather[0].main);
+
+        var tempData = document.createElement("p");
+        tempData.textContent = "Temp: " + Math.round(data.daily[i].temp.day) + " °C";
+
+        var windData = document.createElement("p");
+        windData.textContent = "Wind: " + data.daily[i].wind_speed + " m/s";
+
+        var humidityData = document.createElement("p");
+        humidityData.className = "col-md-6";
+        humidityData.textContent = "Humidity: " + data.daily[i].humidity + " %";
+
+        weatherIconEl.appendChild(weatherIcon);
+        forecastCardEl.append(dateEl, weatherIconEl, tempData, windData, humidityData);
+        forecastEl.appendChild(forecastCardEl);
+    }
+};
+
+var displaySearchHistory = function (city) {
+    var searchHistoryBtn = document.createElement("button");
+    searchHistoryBtn.className = "search-history-button";
+    searchHistoryBtn.setAttribute("type", "submit");
+    searchHistoryBtn.textContent = city;
+    searchHistoryEl.appendChild(searchHistoryBtn);
+};
+
+var addNewSearchHistory = function (data) {
+    var city = data[0].name.toUpperCase();
+    var matchIndex = searchHistory.indexOf(city);
+
+    if (matchIndex === -1 & searchHistory.length <=9) {
+        searchHistory.push(city);
+        displaySearchHistory(city);
+        return;
+    } else if (matchIndex !== -1) {
+        array.push(array.splice(matchIndex, 1)[0]);
+    } else {
+        searchHistory.shift();
+        searchHistory.push(city);
+    }
+
+    for (var i = 0; i < searchHistory.length; i++) {
+        displaySearchHistory(searchHistory[i]);
+    }
+    localStorage.setItem("search", searchHistory);
+}
+
+var loadSearchHistory = function () {
+    searchHistory = localStorage.getItem("search");
+    console.log(searchHistory)
+    if (!searchHistory) {
+        searchHistory = [];
+    } else {
+        searchHistory = JSON.parse(searchHistory);
+        for (var i = 0; i < searchHistory.length; i++) {
+            displaySearchHistory(searchHistory[i]);
+        };
+    };
+};
+
+loadSearchHistory()
 userFormEl.addEventListener("submit", formSubmitHandler);
 
